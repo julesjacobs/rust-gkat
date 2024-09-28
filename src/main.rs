b12a6036fbf8c725eaa190e17fa8127719acb018
qcfu <qcfu@bu.edu>
2024-09-28 01:17:08 -0400

    ok

--- src/main.rs
+++ src/main.rs
@@ -18,26 +18,7 @@ fn main() {
     let exp1 = exp1.to_hashcons(&mut nb, &mut fb, &mut fp);
     let exp2 = exp2.to_hashcons(&mut nb, &mut fb, &mut fp);
     let result = equiv(&mut fb, &mut fp, &exp1, &exp2);
-    println!("equiv_result = {}", result);
+    println!("equiv_expected = {}", b);
+    println!("equiv_result   = {}", result);
     assert!(b == result);
-}
-
-#[test]
-fn test_main() {
-    let paths = fs::read_dir("./dataset/1000").unwrap();
-    for p in paths {
-        let p = p.unwrap().path();
-        let mut nb: NameBuilder = NameBuilder::new();
-        let mut fb: HConsign<BExp_> = HConsign::empty();
-        let mut fp: HConsign<Exp_> = HConsign::empty();
-        println!("{}", &p.to_str().unwrap());
-        let file = fs::read_to_string(p).expect("cannot read file");
-        let (exp1, exp2, b) = parse(file);
-        let exp1 = exp1.to_hashcons(&mut nb, &mut fb, &mut fp);
-        let exp2 = exp2.to_hashcons(&mut nb, &mut fb, &mut fp);
-        let result = equiv(&mut fb, &mut fp, &exp1, &exp2);
-        println!("equiv_expected = {}", b);
-        println!("equiv_result   = {}", result);
-        assert!(b == result);
-    }
 }
