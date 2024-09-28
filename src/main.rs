2fa62eaf77cf935dd8d441903a30f48760ca727b
qcfu <qcfu@bu.edu>
2024-09-27 22:25:34 -0400

    ok

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,20 @@ use parsing::parse;
 use std::{env, fs};
 
 fn main() {
-    test_main();
-    // let mut nb: NameBuilder = NameBuilder::new();
-    // let mut fb: HConsign<BExp_> = HConsign::empty();
-    // let mut fp: HConsign<Exp_> = HConsign::empty();
-    // let args: Vec<String> = env::args().collect();
-    // let file = fs::read_to_string(&args[1]).expect("cannot read file");
-    // let (exp1, exp2, b) = parse(file);
-    // let exp1 = exp1.to_hashcons(&mut nb, &mut fb, &mut fp);
-    // let exp2 = exp2.to_hashcons(&mut nb, &mut fb, &mut fp);
-    // let result = equiv(&mut fb, &mut fp, &exp1, &exp2);
-    // println!("equiv_result = {}", result);
-    // assert!(b == result);
+    let mut nb: NameBuilder = NameBuilder::new();
+    let mut fb: HConsign<BExp_> = HConsign::empty();
+    let mut fp: HConsign<Exp_> = HConsign::empty();
+    let args: Vec<String> = env::args().collect();
+    let file = fs::read_to_string(&args[1]).expect("cannot read file");
+    let (exp1, exp2, b) = parse(file);
+    let exp1 = exp1.to_hashcons(&mut nb, &mut fb, &mut fp);
+    let exp2 = exp2.to_hashcons(&mut nb, &mut fb, &mut fp);
+    let result = equiv(&mut fb, &mut fp, &exp1, &exp2);
+    println!("equiv_result = {}", result);
+    assert!(b == result);
 }
 
-// #[test]
+#[test]
 fn test_main() {
     let paths = fs::read_dir("./dataset/1000").unwrap();
     for p in paths {
