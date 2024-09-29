842769111cf0f2d7312ca8e719ac16b3276142e1
qcfu <qcfu@bu.edu>
2024-09-29 11:01:48 -0400

    ok

--- src/kernel/manager.rs
+++ src/kernel/manager.rs
@@ -1,25 +1,23 @@
 use super::*;
-use ahash::AHasher;
+use ahash::{HashMap, HashSet};
 use disjoint_sets::UnionFindNode;
 use hashconsing::HConsign;
 use rsdd::builder::BottomUpBuilder;
 use rsdd::repr::BddPtr;
-use std::collections::{HashMap, HashSet};
-use std::hash::BuildHasherDefault;
 
 pub struct GkatManager<'a, Builder: BottomUpBuilder<'a, BddPtr<'a>>> {
     // bexp states
     pub(super) name_stamp: u64,
-    pub(super) name_map: HashMap<String, u64, BuildHasherDefault<AHasher>>,
+    pub(super) name_map: HashMap<String, u64>,
     pub(super) bexp_hcons: HConsign<BExp_>,
     pub(super) bexp_builder: &'a Builder,
-    pub(super) bexp_cache: HashMap<BExp, BddPtr<'a>, BuildHasherDefault<AHasher>>,
+    pub(super) bexp_cache: HashMap<BExp, BddPtr<'a>>,
     // exp states
     pub(super) exp_hcons: HConsign<Exp_>,
     // search states
-    pub(super) dead_states: HashSet<Exp, BuildHasherDefault<AHasher>>,
-    pub(super) explored: HashSet<Exp, BuildHasherDefault<AHasher>>,
-    pub(super) uf_table: HashMap<Exp, UnionFindNode<()>, BuildHasherDefault<AHasher>>,
+    pub(super) dead_states: HashSet<Exp>,
+    pub(super) explored: HashSet<Exp>,
+    pub(super) uf_table: HashMap<Exp, UnionFindNode<()>>,
 }
 
 impl<'a, Builder: BottomUpBuilder<'a, BddPtr<'a>>> GkatManager<'a, Builder> {
